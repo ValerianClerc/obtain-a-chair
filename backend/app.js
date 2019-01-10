5a14ba4bd92d0d666208cb2148977cb6e04378e7
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2019-01-10 18:56:13 -0500

    split up routes to different functionalities

--- backend/app.js
+++ backend/app.js
@@ -10,82 +10,84 @@ app.use(
     })
 );
 
-app.post("/request", (req, res) => {
+app.post("/getCourse", (req, res) => {
     let testJSON = {
         tempkey: "tempvalue"
     };
 
-    console.log(req.body);
     let password = req.body.pass;
     let username = req.body.user;
+
+    let courseInfo = {
+        dep: req.body.subject,
+        number: req.body.classNum,
+        season: req.body.season,
+        year: req.body.year
+    };
+
     console.log(`User : ${username}`);
     console.log(`Pass : ${password}`);
 
-    let minerva = new Minerva(username, password); // or store 'em in environment MG_USER & MG_PASS
+    console.log(`Course Info: ${courseInfo}`);
 
-    //   minerva
-    //     .getTranscript()
-    //     .then(function(transcript) {
-    //       console.log(transcript);
-    //     })
-    //     .catch(function(err) {
-    //       console.log(err);
-    //     });
-    let courseInfo = {
-        dep: "MATH",
-        number: "340",
-        season: "w",
-        year: "2019"
-    };
+    let minerva = new Minerva(username, password); // or store 'em in environment MG_USER & MG_PASS
 
     minerva
         .getCourses(courseInfo)
         .then(function (courses) {
             console.log("MADE IT");
             console.log(courses);
-            // => [{
-            //  is_full false,
-            //  crn: '709',
-            //  department: 'COMP',
-            //  course_number: '250',
-            //  type: 'Lecture',
-            //  days: ['MWF'],
-            //  time: ['09:35 AM-10:25 AM'],
-            //  instructor: 'Martin Robillard',
-            //  status: 'Active'
-            //  },{
-            //    ...
-            //  }]
+            res.json(courses);
         })
         .catch(reject => {
             console.log("FAILED");
             console.log(reject);
+            res.json(reject);
         });
 
-    newCourse = {
-        season: "w",
-        year: "2019",
-        crn: "611"
-    }
-    // minerva.addCourses(newCourse)
-    //     .then((resolve) => {
-    //         console.log(resolve)
-    //     })
-    //     .catch((reject) => {
-    //         console.log(reject)
-    //     })
+   
+    
+});
 
-    setTimeout(() => {
-        minerva
-            .getTranscript()
-            .then(function (transcript) {
-                console.log(transcript);
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-    }, 10000)
-    res.json(testJSON);
+
+app.post('/addCourse', (req, res) => {
+    newCourse = {
+        season: req.body.season,
+        year: req.body.year,
+        crn: req.body.crn
+    };
+    minerva.addCourses(newCourse)
+        .then((resolve) => {
+            console.log(resolve);
+            res.json(resolve);
+        })
+        .catch((reject) => {
+            console.log(reject);
+            res.json(reject);
+        });
+});
+
+
+app.post('/getTranscript', (req, res) => {
+
+    let password = req.body.pass;
+    let username = req.body.user;
+
+    console.log(`User : ${username}`);
+    console.log(`Pass : ${password}`);
+
+    let minerva = new Minerva(username, password); // or store 'em in environment MG_USER & MG_PASS
+
+    minerva.getTranscript()
+    .then(function(transcript) {
+        console.log(transcript);
+        res.json(transcript);
+    })
+    .catch(function(err) {
+        console.log(err);
+        res.json(err);
+    });
+
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
