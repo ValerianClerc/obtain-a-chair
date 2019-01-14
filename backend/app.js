da1838c3df89890f25051b54e76835cd696c9db2
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2019-01-14 00:10:18 -0500

    frontend init

--- backend/app.js
+++ backend/app.js
@@ -1,106 +1,104 @@
 const Minerva = require("mcgill-minerva-api"),
-    express = require("express"),
-    bodyParser = require("body-parser");
+  express = require("express"),
+  bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
+  bodyParser.urlencoded({
+    extended: true
+  })
 );
 
 app.post("/getCourse", (req, res) => {
-    let testJSON = {
-        tempkey: "tempvalue"
-    };
+  let testJSON = {
+    tempkey: "tempvalue"
+  };
 
-    let password = req.body.pass;
-    let username = req.body.user;
+  let password = req.body.pass;
+  let username = req.body.user;
 
-    let courseInfo = {
-        dep: req.body.subject,
-        number: req.body.classNum,
-        season: req.body.season,
-        year: req.body.year
-    };
+  let courseInfo = {
+    dep: req.body.subject,
+    number: req.body.classNum,
+    season: req.body.season,
+    year: req.body.year
+  };
 
-    console.log(`User : ${username}`);
-    console.log(`Pass : ${password}`);
+  console.log(`User : ${username}`);
+  console.log(`Pass : ${password}`);
 
-    console.log(`Course Info: ${courseInfo}`);
+  console.log(`Course Info: ${courseInfo}`);
 
-    let minerva = new Minerva(username, password); // or store 'em in environment MG_USER & MG_PASS
+  let minerva = new Minerva(username, password);
 
-    minerva
-        .getCourses(courseInfo)
-        .then(function (courses) {
-            console.log("Course retrieved");
-            console.log(courses);
-            res.json(courses);
-        })
-        .catch(reject => {
-            console.log("Unable to get course");
-            console.log(reject);
-            res.json(reject);
-        });
-
-
-
+  minerva
+    .getCourses(courseInfo)
+    .then(function(courses) {
+      console.log("Course retrieved");
+      console.log(courses);
+      res.json(courses);
+    })
+    .catch(reject => {
+      console.log("Unable to get course");
+      console.log(reject);
+      res.json(reject);
+    });
 });
 
+app.post("/addCourse", (req, res) => {
+  let password = req.body.pass;
+  let username = req.body.user;
 
-app.post('/addCourse', (req, res) => {
+  let minerva = new Minerva(username, password);
 
-    let password = req.body.pass;
-    let username = req.body.user;
-
-    let minerva = new Minerva(username, password); // or store 'em in environment MG_USER & MG_PASS
-
-    newCourse = {
-        season: req.body.season,
-        year: req.body.year,
-        crn: req.body.crn
-    };
-    if (newCourse.season == null || newCourse.year == null || newCourse.crn == null) {
-        res.status(400).json({
-            msg: "Season, Year, or CRN was left empty"
-        })
-    }
-    minerva.addCourses(newCourse)
-        .then((resolve) => {
-            console.log('Course add attempted successfully')
-            console.log(resolve);
-            res.json(resolve);
-        })
-        .catch((reject) => {
-            console.log('Unable to try to add course')
-            console.log(reject);
-            res.json(reject);
-        });
+  newCourse = {
+    season: req.body.season,
+    year: req.body.year,
+    crn: req.body.crn
+  };
+  if (
+    newCourse.season == null ||
+    newCourse.year == null ||
+    newCourse.crn == null
+  ) {
+    res.status(400).json({
+      msg: "Season, Year, or CRN was left empty"
+    });
+  }
+  minerva
+    .addCourses(newCourse)
+    .then(resolve => {
+      console.log("Course add attempted successfully");
+      console.log(resolve);
+      res.json(resolve);
+    })
+    .catch(reject => {
+      console.log("Unable to try to add course");
+      console.log(reject);
+      res.json(reject);
+    });
 });
 
+app.post("/getTranscript", (req, res) => {
+  let password = req.body.pass;
+  let username = req.body.user;
 
-app.post('/getTranscript', (req, res) => {
+  console.log(`User : ${username}`);
+  console.log(`Pass : ${password}`);
 
-    let password = req.body.pass;
-    let username = req.body.user;
+  let minerva = new Minerva(username, password);
 
-    console.log(`User : ${username}`);
-    console.log(`Pass : ${password}`);
-
-    let minerva = new Minerva(username, password); // or store 'em in environment MG_USER & MG_PASS
-
-    minerva.getTranscript()
-        .then(function (transcript) {
-            console.log(transcript);
-            res.json(transcript);
-        })
-        .catch(function (err) {
-            console.log(err);
-            res.json(err);
-        });
-
+  minerva
+    .getTranscript()
+    .then(function(transcript) {
+      console.log(transcript);
+      res.json(transcript);
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.json(err);
+    });
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
